f1e3f4f241743554c0a40b6d475f1275c10e4d96
Clement-Julia <81628168+Clement-Julia@users.noreply.github.com>
2023-05-23 15:05:14 +0200

    feat: add basic function

--- src/components/routes/Routeur.js
+++ src/components/routes/Routeur.js
@@ -2,15 +2,17 @@ import React from 'react'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import Navigation from '../layout/navigation'
 
-// import Home from '../pages/home';
+import NotFound from './error-page'
+import Home from '../pages/Home'
 
 const Routeur = () => {
 	return (
 		<BrowserRouter>
-            <Navigation/>
-                <Routes>
-                    <Route exact path='/' element={<Home/>} />
-                </Routes>
+			<Navigation />
+			<Routes>
+				<Route exact path='/' element={<Home />} />
+				<Route path='*' element={<NotFound />} />
+			</Routes>
 		</BrowserRouter>
 	)
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Routeur from './components/routes/Routeur'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Routeur />
   </React.StrictMode>
 );
 

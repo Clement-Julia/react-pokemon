2de2c92ffcb230048ec9b16800f792669a28e225
r-tisba <81628282+r-tisba@users.noreply.github.com>
2023-05-23 17:24:30 +0200

    feat: details pokemon & css

--- src/components/pages/CaracPokemon.js
+++ src/components/pages/CaracPokemon.js
@@ -9,23 +9,41 @@ const CaracPokemon = () => {
     const pokemon = pokemons.find((pokemon) => pokemon.id === parseInt(id));
 
     if (!pokemon) {
-        return <div>Pokemon non trouvé</div>;
+        return <div className='text-center mt-5 titre'>Pokemon non trouvé</div>;
     }
 
     const { nom, type, pv, stats, vitesse } = pokemon;
 
-    return (
-        <div className='container d-flex justify-content-center'>
-            <div className='card p-4'>
-                <h2 className=''>{nom}</h2>
+    var idPokemon = id
+    if (id > 10 & id < 100) {
+        idPokemon = '0' + idPokemon
+    }
+    if (id < 10) {
+        idPokemon = '00' + idPokemon
+    }
 
-                <div>Type : {type}</div>
-                <div>PV : {pv}</div>
-                <div>Attaque : {stats.attaque}</div>
-                <div>Att Spé : {stats.att_spe}</div>
-                <div>Défense : {stats.defense}</div>
-                <div>Déf Spé : {stats.def_spe}</div>
-                <div>Vitesse : {vitesse}</div>
+    var lien = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/" + idPokemon + ".png";
+    var backColor = "type-" + type[0].toLowerCase()
+    var cardClass = "card cardPokemon p-4 " + backColor
+    return (
+        <div className='container d-flex justify-content-center my-4'>
+            <div className={cardClass}>
+                <div className='cardHeader'>
+                    <div className='nomPokemon'>{nom}</div>
+                    <div className='pv'><span className="petit me-2">PV</span>{pv}</div>
+                </div>
+                <div className='cardImg'>
+                    <img className='imgPokemon' src={lien}></img>
+                </div>
+                <div className='typePokemon'>Type : {type.join(', ')}</div>
+                <div className="statsPokemon mt-3 mb-2">
+                    <div>Attaque : {stats.attaque}</div>
+                    <div>Défense : {stats.defense}</div>
+                    <div>Att Spé : {stats.att_spe}</div>
+                    <div>Déf Spé : {stats.def_spe}</div>
+                </div>
+
+                <div className='vitessePokemon'>Vitesse : {vitesse}</div>
 
             </div>
         </div>
